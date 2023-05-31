359bbcba525e34249e5021a4ad0a68b644939e01
Daniel Lam <Dann.lam@gmail.com>
2023-05-31 01:13:44 -0700

    Form now works

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,22 +1,64 @@
-import React from 'react';
+import React from "react";
 
-export default function Contact() {
+export default function Contact(props) {
+  const handleEvent = (e) => {
+    if (e.target.name === "name") {
+      if (!e.target.value) {
+        alert("Yenter a name.");
+      }
+    } else if (e.target.name === "email") {
+      if (e.target.value === "") {
+        alert("enter an email.");
+      } else if (
+        !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
+          String(e.target.value).toLowerCase()
+        )
+      ) {
+        alert("enter a valid email adress.");
+      }
+    } else if (e.target.name === "message") {
+      if (!e.target.value) {
+        alert("enter a message.");
+      }
+    }
+  };
+
   return (
     <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <form>
+        <div>
+          <h1>Contact me!</h1>
+          <label htmlFor="name-input" id="text_big">
+            Name
+          </label>
+          <input
+            id="name-input"
+            name="name"
+            onBlur={handleEvent}
+            type="text"
+            // value={name}
+          />
+        </div>
+        <div>
+          <label htmlFor="email-input">Email</label>
+          <input
+            id="email-input"
+            name="email"
+            onBlur={handleEvent}
+            type="text"
+          />
+        </div>
+        <div>
+          <label htmlFor="message-input">Message</label>
+          <textarea
+            id="message-input"
+            name="message"
+            onBlur={handleEvent}
+            type="text"
+          ></textarea>
+        </div>
+        <input type="submit" />
+      </form>
     </div>
   );
 }
