6df2f1172d6d0a2f6cec4c7dbc7e7e1ef4318289
Daniel Lam <Dann.lam@gmail.com>
2023-05-31 22:34:46 -0700

    Fixed bad link

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -46,7 +46,7 @@ function NavTabs({ currentPage, handlePageChange }) {
           // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === "Resume" ? "nav-link active" : "nav-link"}
         >
-          Contact
+          Resume
         </a>
       </li>
     </ul>
